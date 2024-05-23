08a8e513b6197eafb899c719a316b31fb63e6187
Pandit Alabi <pandit.alabi@amalitech.com>
2024-05-23 15:03:59 +0000

    Added and fixed all bug by God's Grace

---
+++ src/app/DataTypes.interface.ts
@@ -0,0 +1,6 @@
+export interface Questions {
+    id: string;
+    question : string; 
+    answer: string;
+    votes : number;
+}
---
+++ src/app/questionaire/questionaire.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Questions } from '../DataTypes.interface';
+import { NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-questionaire',
+  standalone: true,
+  imports: [ NgFor, ReactiveFormsModule , FormsModule],
+  templateUrl: './questionaire.component.html',
+  styleUrl: './questionaire.component.css'
+})
+export class QuestionaireComponent  {
+
+  
+
+  form: FormGroup;
+  ListQuestion : any[] = [];
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      questions: this.fb.array([]),
+    });
+  }
+  
+  get questions () {
+    return this.form.get('questions') as FormArray;
+  }
+
+  addCreds() {
+    this.questions.push( new FormControl<string>(''))
+  }
+  
+
+  publish(){
+    console.log(this.questions.value)
+
+    const newArray = [];
+
+    for(let question of this.questions.value ){
+      const list : any = {
+        question,
+      }
+
+      newArray.push(list)
+    }
+
+    this.ListQuestion = newArray;
+
+    console.log(this.ListQuestion)
+  }
+  
+  adjustTextareaHeight(event: any): void {
+    const textarea: HTMLTextAreaElement = event.target;
+    textarea.style.height = 'auto'; // Reset the height
+    textarea.style.height = `${textarea.scrollHeight}px`; // Set the new height
+  }
+
+  addToList(){
+     this.addCreds();
+  }
+
+  remove(index : number) {
+
+    if (index > -1 && index < this.questions.value.length && this.questions.value.length !== 1) {
+      this.questions.removeAt(index);
+      console.log(index)
+    }
+  }
+
+  trackByFn(index: any, item: Questions) {
+    return index; // or item.id if each item has a unique id
+  }
+
+
+}
